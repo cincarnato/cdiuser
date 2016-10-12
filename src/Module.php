c108be8e6a56a165f46e8cc0dfa82538ecb77022
cristian <cristian@192.168.12.123>
2016-10-12 15:37:05 -0300

    Add zfc-user-doctrine-orm

--- src/Module.php
+++ src/Module.php
@@ -5,21 +5,12 @@ namespace CdiUser;
 class Module {
 
     public function getConfig() {
-        return include __DIR__ . '/../../config/module.config.php';
+        return include __DIR__ . '/../config/module.config.php';
     }
 
-    public function getAutoloaderConfig() {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/../../src/' . __NAMESPACE__,
-                ),
-            ),
-        );
-    }
 
     public function getServiceConfig() {
-        return include __DIR__ . '/../../config/services.config.php';
+        return include __DIR__ . '/../config/services.config.php';
     }
 
 
